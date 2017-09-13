a0f9a777abcab0a07fd5f7b8ad5a6be68c6c1894
Rotem Tamir <rotemtamir@getnexar.com>
2017-09-13 15:36:33 +0300

    v2

--- src/back-server/app.js
+++ src/back-server/app.js
@@ -1,10 +1,27 @@
 const Koa = require('koa');
 const app = new Koa();
+const sleep = require('sleep-promise');
+const fs = require('mz/fs');
+const SHARED_FILE = process.env.SHARED_FILE || '/tmp/shared.txt';
 
 app.use(async ctx => {
   ctx.body = 'Hi!!!';
 });
 
+
+
 console.log('back service on 4000')
 app.listen(4000);
 
+setTimeout(async () => {
+  while(true) {
+    console.log('randomizing')
+    let x = rnd();
+    await fs.writeFile(SHARED_FILE, x)
+    await sleep(3000)
+  }
+}, 0)
+
+function rnd() {
+  return Math.floor(Math.random() * 1000)
+}
--- src/server/app.js
+++ src/server/app.js
@@ -1,13 +1,14 @@
 const Koa = require('koa');
 const app = new Koa();
 const request = require('axios');
-
+const fs = require('mz/fs');
+const SHARED_FILE = process.env.SHARED_FILE || '/tmp/shared.txt';
 
 app.use(async ctx => {
   let res = await request.get('http://localhost:4000');
-  ctx.body = `Hello World. Back says(${res.data})`;
+  let rdm = await fs.readFile(SHARED_FILE)
+  ctx.body = `Hello World. Back says(${res.data}). A random file says: ${rdm}`;
 });
 
 console.log('listening on 3000');
 app.listen(3000);
-
